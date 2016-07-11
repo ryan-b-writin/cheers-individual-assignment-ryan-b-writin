794bb65a3d4e940ab5abd4e2a3ba79db5f9c8898
Ryan B <ryan.bliton@gmail.com>
2016-07-11 18:42:22 -0500

    changed writeLine text

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace Cheers
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("What's your name?");
         }
     }
 }
