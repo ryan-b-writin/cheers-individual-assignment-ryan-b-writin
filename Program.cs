92b401a4bbb257c4a830a08a2769c6f34d305b35
Ryan B <ryan.bliton@gmail.com>
2016-07-11 20:21:37 -0500

    program now spits letters back at you

--- Program.cs
+++ Program.cs
@@ -13,6 +13,10 @@ namespace Cheers
             Console.Write("> ");
             String user_name = Console.ReadLine();
 
+            for(int i = 0; i < user_name.Length; i++) {
+                Console.WriteLine("give me a ..." + user_name[i]);
+            }
+
             Console.WriteLine(user_name.ToUpper() + " is... GRRRREAT!");
         }
     }
